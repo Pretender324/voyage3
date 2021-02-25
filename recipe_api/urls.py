e872a001a1394ff307704e1a8137011236327d76
Pretender324 <r1kuchicken5761@gmail.com>
2021-02-25 10:14:44 +0900

    heroku deploy

--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from recipe.urls import router
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
