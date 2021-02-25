699b233fba21f103c5f05ca54060eb2f610d0596
Pretender324 <r1kuchicken5761@gmail.com>
2021-02-25 12:10:29 +0900

    heroku deploy

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
 from .views import RecipeViewSet
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'recipes', RecipeViewSet)
